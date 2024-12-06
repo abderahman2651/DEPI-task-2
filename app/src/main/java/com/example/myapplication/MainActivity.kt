0f2e750cfe0942b04695d4b933f1a4cf774032e9
abderahman2651 <abderahmanmahmoud2651@gmail.com>
2024-12-06 15:37:37 +0200

    createnewscreen

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.view.View.OnClickListener
@@ -11,7 +12,10 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
- class MainActivity : AppCompatActivity() , View.OnClickListener {
+ class MainActivity : AppCompatActivity() {
+
+     val username : String = "abderahman"
+     val password : String = "abdo123abdo"
 
     lateinit var etd_username: EditText
     lateinit var etd_password: EditText
@@ -25,13 +29,29 @@ import androidx.core.view.WindowInsetsCompat
         etd_username = findViewById(R.id.edt_username)
         etd_password = findViewById(R.id.edt_password)
         btn_login = findViewById(R.id.btn_login)
-        btn_login.setOnClickListener(this)
+        btn_login.setOnClickListener {
+            if(!etd_username.text.toString().isNullOrEmpty() && !etd_password.text.toString().isNullOrEmpty()){
+                if (etd_username.text.toString().equals(username) && etd_password.text.toString().equals(password)){
+                    Toast.makeText(this , "Login successful" , Toast.LENGTH_LONG).show()
 
+                    var intent:Intent = Intent(this , secondActivity::class.java)
+                    intent.putExtra("username" , etd_username.text.toString())
+                    startActivity(intent)
+                }
+            }
+        }
     }
 
-     override fun onClick(v: View?) {
-         Toast.makeText(this , "Login Button Clicked" , Toast.LENGTH_LONG).show()
-     }
+//     override fun onClick(v: View?) {
+//
+//         when(v!!.id){
+//             R.id.btn_login -> {
+//                 Toast.makeText(this , "Login Button Clicked" , Toast.LENGTH_LONG).show()
+//             }
+//         }
+//
+//
+//     }
 
 //    fun onClicklogin(view : View){
 //        Toast.makeText(this , "Login Button Clicked" , Toast.LENGTH_LONG).show()
---
+++ app/src/main/java/com/example/myapplication/secondActivity.kt
@@ -0,0 +1,21 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class secondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_second)
+0
+        val username = intent.extras!!.getString("username")
+
+        Toast.makeText(this, "Hello $username", Toast.LENGTH_SHORT).show()
+
+    }
+}
